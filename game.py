a81b7a3cd3f6b2f080be6c668a255d3e1ccf9bca
OboDerf <OboDerf@gmail.com>
2018-10-17 13:48:56 +0100

    Update Game.py to add in the translation function
    Also fixed the game classes' variables

--- game.py
+++ game.py
@@ -3,9 +3,11 @@ from gameparser import *
 import sys
 
 import en_map
+import en_translation
 #import en_items - in case these are ever created
 #import en_function
 import we_map
+import we_translation
 #import we_items
 #import we_function
 
@@ -14,7 +16,7 @@ import we_map
 #Globals are considered dirty, but that isn't going to stop me using them!
 eng_lang = True
 rooms = en_map.rooms
-incorrect_answer = "\nPlease enter a valid answer\n"# For validation use 'incorrect_answer' as it's bilingual
+translation = en_translation
 
 
 ## Classes
@@ -31,62 +33,53 @@ class Player:
 class Game:
 
     def __init__(self):
-        self.difficulty = 'medium' # This is setting all the default variables. I'm assuming medium until user says otherwise
-        self.max_attempts = 60
-        self.keys_max = 5
-        self.keys_avaliable = 5
-        self.keys_needed = 3
+        self.difficulty = 1 # This is setting all the default variables.I'm assuming medium until user says otherwise
+        self.difficulty_stats = {
+                0 : {
+                    'attempts_max': 100,
+                    'keys_max': 6,
+                    'keys_avaliable': 6,
+                    'keys_needed': 2},
+                1 : {
+                    'attempts_max': 80,
+                    'keys_max': 5,
+                    'keys_avaliable': 5,
+                    'keys_needed': 3},
+                
+                2 : {
+                    'attempts_max': 60,
+                    'keys_max': 4,
+                    'keys_avaliable': 4,
+                    'keys_needed': 4},
+
+                3 : {
+                    'attempts_max': 50,
+                    'keys_max': 6,
+                    'keys_avaliable': 6,
+                    'keys_needed': 6}        
+            }
         self.game_lost = False
         self.game_won = False
         
-    def difficulty_calc(self): # If the difficulty is 'e', 'm', 'h', or 'i' it edits the game settings
-        # Note: I know there's a better way of doing this, but this is easy so bugger it
-        if self.difficulty == 'easy' or self.difficulty == 'hawdd':
-            self.max_attempts = 100
-            self.keys_max = 6
-            self.keys_avaliable = 6
-            self.keys_needed = 2
-        elif self.difficulty == 'medium' or self.difficulty == 'canolig':
-            self.max_attempts = 80
-            self.keys_max = 5
-            self.keys_avaliable = 5
-            self.keys_needed = 3
-        elif self.difficulty == 'hard' or self.difficulty == 'caled':
-            self.max_attempts = 60
-            self.keys_max = 4
-            self.keys_avaliable = 4
-            self.keys_needed = 4
-        elif self.difficulty == 'impossible' or self.difficulty == 'amhosibl':
-            self.max_attempts = 50
-            self.keys_max = 6
-            self.keys_avaliable = 6
-            self.keys_needed = 6
-        else: print("If you ever see this, Kai's an idiot")
-
     def select_difficulty(self):
-        if eng_lang:
-            a = normalise_str_input(input("What difficulty would you like?\n - Easy\n - Medium\n - Hard\n - Impossible\n")) 
-            if a in ['easy', 'medium', 'hard', 'impossible']: self.difficulty = a
-            else: print(incorrect_answer)
-        else:
-            a = normalise_str_input(input("Pa anhawster yr hoffech chi?\n - Hawdd\n - Canolig\n - Caled\n - Amhosibl\n")) 
-            if a in ['hawdd', 'canolig', 'caled', 'amhosibl']: self.difficulty = a
-            else: print(incorrect_answer)
+        a = normalise_str_input(input("What difficulty would you like?\n - Easy\n - Medium\n - Hard\n - Impossible\n")) 
+        if a in ['easy', 'medium', 'hard', 'impossible']: self.difficulty = a
+        else: print(translation["incorrect_answer"])
         self.difficulty_calc()
 
 
 ## Main Code
 
 def toggle_lang(): # Does what it says on the tin. Takes the defined globals and toggles them between versions
-    global eng_lang, rooms, incorrect_answer
+    global eng_lang, rooms, incorrect_answer, tranlation
     if eng_lang:
         eng_lang = False
         rooms = we_map.rooms
-        incorrect_answer = "\nRhowch ateb dilys.\n"
+        translation = we_translation
     else:
         eng_lang = True
         rooms = en_map.rooms
-        incorrect_answer = "\nPlease enter a valid answer\n"
+        translation = en_translation
     return rooms
 
 
@@ -106,9 +99,9 @@ def main_menu(game, player):
             print("TEMP DDEWISLEN")
             print("----------\n")
             print("Nodwch _ i:\n")
-            print("1. I paly Gêm")
+            print("1. I chwarae Gêm")
             print("2. Toggle: English \ Cymraeg")
-            print("3. I dewiswch anhawster (Ar hyn o bryd " + game.difficulty + ")")
+            print("3. I ddewis anhawster (Ar hyn o bryd " + game.difficulty + ")")
             print("0. I ymadael\n")
 
         choice = normalise_str_input(input())
--- temp.py
+++ temp.py
@@ -1,18 +1,84 @@
+eng_lang = True
+incorrect_answer = "\nPlease enter a valid answer\n"
 
-temp = "hello"
+class Game:
 
-def menu2():
-    print(temp)
+    def __init__(self):
+        self.difficulty = 'm' # This is setting all the default variables.I'm assuming medium until user says otherwise
+        self.difficulty_stats = {
+                "e" : {
+                    'attempts_max': 100,
+                    'keys_max': 6,
+                    'keys_avaliable': 6,
+                    'keys_needed': 2},
+                "m" : {
+                    'attempts_max': 80,
+                    'keys_max': 5,
+                    'keys_avaliable': 5,
+                    'keys_needed': 3},
+                
+                "h" : {
+                    'attempts_max': 60,
+                    'keys_max': 4,
+                    'keys_avaliable': 4,
+                    'keys_needed': 4},
 
-def menu3(temp):
-    print(temp)
+                "i" : {
+                    'attempts_max': 50,
+                    'keys_max': 6,
+                    'keys_avaliable': 6,
+                    'keys_needed': 6}        
+            }
+        self.game_lost = False
+        self.game_won = False
 
-def main():
-    global temp
-    print(temp)
-    temp = "hi"
-    menu2()
-    menu3(temp)
+    def select_difficulty(self):
+        if eng_lang:
+            a = (input("What difficulty would you like?\n - Easy\n - Medium\n - Hard\n - Impossible\n")) 
+            if a in ['easy', 'medium', 'hard', 'impossible']: self.difficulty = a
+            else: print(incorrect_answer)
+        else:
+            a = (input("Pa anhawster yr hoffech chi?\n - Hawdd\n - Canolig\n - Caled\n - Amhosibl\n")) 
+            if a in ['hawdd', 'canolig', 'caled', 'amhosibl']: self.difficulty = a
+            else: print(incorrect_answer)
 
-if __name__ == "__main__":
-    main()
+def toggle_lang(): # Does what it says on the tin. Takes the defined globals and toggles them between versions
+    global eng_lang, incorrect_answer
+    if eng_lang:
+        eng_lang = False
+        incorrect_answer = "\nRhowch ateb dilys.\n"
+    else:
+        eng_lang = True
+        incorrect_answer = "\nPlease enter a valid answer\n"
+
+def main_menu(game):
+    while True:
+        if eng_lang:
+            print("----------")
+            print("TEMP MENU")
+            print("----------\n")
+            print("Enter _ to:\n")
+            print("1. To play game")
+            print("2. Toggle: English \ Cymraeg")
+            print("3. To pick difficulty (Currently " + game.difficulty + ")")
+            print("0. To exit\n")
+        else:
+            print("----------")
+            print("TEMP DDEWISLEN")
+            print("----------\n")
+            print("Nodwch _ i:\n")
+            print("1. I chwarae Gêm")
+            print("2. Toggle: English \ Cymraeg")
+            print("3. I ddewis anhawster (Ar hyn o bryd " + game.difficulty + ")")
+            print("0. I ymadael\n")
+
+        choice = (input())
+        if choice == '1': break
+        elif choice == '2': toggle_lang()
+        elif choice == '3': game.select_difficulty()
+        elif choice == '0': break
+        else: print(incorrect_answer)
+
+game = Game()
+main_menu(game)
+
